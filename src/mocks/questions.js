b4faca1732255c41204cc24368d2b4afcc6bb48a
Samuel Baraka <barakasamuel125@gmail.com>
2025-07-29 22:35:07 +0300

    First Commit

---
+++ src/mocks/questions.js
@@ -0,0 +1,16 @@
+const questions = [
+  {
+    id: 1,
+    prompt: "What is 2+2?",
+    answers: ["3", "4", "5", "6"],
+    correctIndex: 1
+  },
+  {
+    id: 2,
+    prompt: "What is the capital of France?",
+    answers: ["Paris", "London", "Berlin", "Madrid"],
+    correctIndex: 0
+  }
+];
+
+export default questions;
